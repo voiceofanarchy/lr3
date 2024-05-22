44f8e78b9e1c7ff8d7a69db0c5800995d6f7a931
voiceofanarchy <73114877+voiceofanarchy@users.noreply.github.com>
2024-05-22 20:04:09 +0300

    math_functions rewritten, test_math_functions updated.

--- math_functions.py
+++ math_functions.py
@@ -3,11 +3,28 @@ def add(num1, num2):
         result = float(num1) + float(num2)
         return result
     except ValueError:
-        return "Invalid input"
+        if not is_float(num1) and not is_float(num2):
+            return "Invalid input: Both inputs are not numbers"
+        elif not is_float(num1):
+            return "Invalid input: First input is not a number"
+        else:
+            return "Invalid input: Second input is not a number"
 
 def subtract(num1, num2):
     try:
         result = float(num1) - float(num2)
         return result
     except ValueError:
-        return "Invalid input"
+        if not is_float(num1) and not is_float(num2):
+            return "Invalid input: Both inputs are not numbers"
+        elif not is_float(num1):
+            return "Invalid input: First input is not a number"
+        else:
+            return "Invalid input: Second input is not a number"
+
+def is_float(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
--- test_math_functions.py
+++ test_math_functions.py
@@ -4,18 +4,24 @@ from math_functions import add, subtract
 class TestMathFunctions(unittest.TestCase):
     
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
-        self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(-1, -1), -2)
+        self.assertEqual(add(2, 3), 5.0)
+        self.assertEqual(add(-1, 1), 0.0)
+        self.assertEqual(add(-1, -1), -2.0)
         self.assertEqual(add(2.5, 3.1), 5.6)
-        self.assertEqual(add('a', 3), "Invalid input")
+        self.assertEqual(add('2', '3'), 5.0)
+        self.assertEqual(add('a', '3'), "Invalid input: First input is not a number")
+        self.assertEqual(add('2', 'b'), "Invalid input: Second input is not a number")
+        self.assertEqual(add('a', 'b'), "Invalid input: Both inputs are not numbers")
     
     def test_subtract(self):
-        self.assertEqual(subtract(5, 3), 2)
-        self.assertEqual(subtract(-1, 1), -2)
-        self.assertEqual(subtract(-1, -1), 0)
+        self.assertEqual(subtract(5, 3), 2.0)
+        self.assertEqual(subtract(-1, 1), -2.0)
+        self.assertEqual(subtract(-1, -1), 0.0)
         self.assertEqual(subtract(5.5, 3.1), 2.4)
-        self.assertEqual(subtract('a', 3), "Invalid input")
+        self.assertEqual(subtract('5', '3'), 2.0)
+        self.assertEqual(subtract('a', '3'), "Invalid input: First input is not a number")
+        self.assertEqual(subtract('5', 'b'), "Invalid input: Second input is not a number")
+        self.assertEqual(subtract('a', 'b'), "Invalid input: Both inputs are not numbers")
 
 if __name__ == '__main__':
     unittest.main()
